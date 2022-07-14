70b2fea8de13c22bb303b28e995ca3b526283b79
Andika Pranayoga <praandikayoga@gmail.com>
2022-07-14 14:22:58 +0800

    edit EntryController

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -435,7 +435,7 @@ class EntryController extends Controller
         $end = $req->end;
         if ($start == null && $end == null) {
             if ($dc == 'group') {
-                $data = Entry::whereBetween('entry_date',[$yes, $today])->get();
+                $data = Entry::orderBy('entry_date','desc')->get();
                 // dd($data);
             }else{
                 $data = Entry::join('stocks','entries.stock_id','stocks.id')
