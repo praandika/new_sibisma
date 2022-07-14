3a8c4fe301d35bb903bc9a5244aadf90d5a8709a
Andika Pranayoga <praandikayoga@gmail.com>
2022-07-14 16:08:29 +0800

    edit function delete OutController

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -454,7 +454,6 @@ class SaleController extends Controller
             $his = StockHistory::where('history_date',$sale_date)
             ->where('dealer_code',$dc)->first();
         }
-        dd("in : ".$in, "out : ".$out, "sale : ".$sale, "sale_qty : ".$sale_qty, "last_stok : ".$lastStock, "history : ".$his);
 
         $his->in_qty = $in;
         $his->out_qty = $out;
