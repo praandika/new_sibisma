2fff89a1e038e4fdf298bbe5c4531aa43d3c723f
Andika Pranayoga <praandikayoga@gmail.com>
2022-07-20 14:19:37 +0800

    add Group Unit Report

--- resources/views/component/unit-report.blade.php
+++ resources/views/component/unit-report.blade.php
@@ -371,7 +371,7 @@
                                 transform: skew(-15deg, 0deg);">
                 <strong>{{ $tahun }}</strong>
                 &nbsp;
-                <span style="background-color: #cf0257; padding: 5px; margin-right: -11px;">
+                <span style="background-color: #816797; padding: 5px; margin-right: -11px;">
                     <strong>{{ $total }}</strong>
                     &nbsp;
                 </span>
