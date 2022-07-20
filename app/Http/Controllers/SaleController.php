2826232124840d48c212950fa7940c8187c3a9d6
Andika Pranayoga <praandikayoga@gmail.com>
2022-07-20 15:44:21 +0800

    edit SaleController @index

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -46,7 +46,8 @@ class SaleController extends Controller
             ->join('leasing','sales.leasing_id','leasings.id')
             ->join('users','sales.created_by','users.id')
             ->where('sale_date',$today)->where('stocks.dealer_id',$did)->orderBy('sales.id','desc')
-            ->select('sales.id','dealers.dealer_code','units.model_name','colors.color_code','colors.color_name','units.year_mc','sales.frame_no','sales.sale_qty','leasings.leasing_code','users.first_name');
+            ->select('sales.id','dealers.dealer_code','units.model_name','colors.color_code','colors.color_name','units.year_mc','sales.frame_no','sales.sale_qty','leasings.leasing_code','users.first_name')
+            ->get();
             return view('page', compact('stock','leasing','today','data','dealer','dealerCode'));
         }
         
