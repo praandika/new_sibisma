93f24ef6876153d772a042118110d79bf5ac8975
Andika Pranayoga <praandikayoga@gmail.com>
2023-01-01 09:16:05 +0800

    Update Widget Stock

--- app/Http/Livewire/WidgetStockQty.php
+++ app/Http/Livewire/WidgetStockQty.php
@@ -18,7 +18,7 @@ class WidgetStockQty extends Component
             $stock = Stock::sum('qty');
             $data = Stock::join('dealers','stocks.dealer_id','=','dealers.id')
             ->selectRaw('SUM(stocks.qty) as stock, dealers.dealer_code, dealers.dealer_name')
-            ->groupBy('stocks.dealer_id')->orderBy('stocks.qty','desc')->get();
+            ->groupBy('stocks.dealer_id')->orderBy('stock','desc')->get();
         } else {
             $stock = Stock::where('dealer_id',$did)->sum('qty');
         }
