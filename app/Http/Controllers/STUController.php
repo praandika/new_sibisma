faf442bf331635d054a920d1d7f354d9452fe7da
Andika Pranayoga <praandikayoga@gmail.com>
2023-01-28 22:23:38 +0800

    Update STU vs Real Page

--- app/Http/Controllers/STUController.php
+++ app/Http/Controllers/STUController.php
@@ -354,8 +354,8 @@ class STUController extends Controller
         // Dalung
         if ($stu_0401 == 0) {
             $vs_0401 = 0*100;
-            dd($stu_0401);
         } else {
+            dd($stu_0401.' / '.$real_0401);
             $get_vs_0401 = ($stu_0401 / $real_0401); 
             $vs_0401 = ($get_vs_0401 - 1)*100;
         }
