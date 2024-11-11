817bd68cf7e4e5a2fc446d5d6c15ff8973f4346b
Andika Pranayoga <praandikayoga@gmail.com>
2024-11-11 14:57:39 +0800

    Update Button Print

--- resources/views/component/button-print.blade.php
+++ resources/views/component/button-print.blade.php
@@ -4,4 +4,4 @@
     @else
         {{ url('report/manpower') }}
     @endif
-" class="btn btn-warning btn-round" style="margin-bottom: 20px;"><i class="fas fa-print"></i>&nbsp;&nbsp; <strong>Print</strong> </a>
+" class="btn {{ Route::is('manpower.*') ? 'btn-light' : 'btn-warning'}} btn-round" style="margin-bottom: 20px;"><i class="fas fa-print"></i>&nbsp;&nbsp; <strong>{{ Route::is('manpower.*') ? 'Export' : 'Print'}}</strong> </a>
