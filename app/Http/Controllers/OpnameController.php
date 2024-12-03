2e67a773eb56cdf447d3a46b3b50f3c8f382c63b
Andika Pranayoga <praandikayoga@gmail.com>
2024-12-03 10:48:37 +0800

    Update Stock Opname Stock Data

--- app/Http/Controllers/OpnameController.php
+++ app/Http/Controllers/OpnameController.php
@@ -26,10 +26,23 @@ class OpnameController extends Controller
         $today = Carbon::now('GMT+8')->format('Y-m-d');
 
         if ($dc == 'group') {
-            $stock = Stock::orderBy('qty','desc')->get();
+            $stock = Stock::join('units','stocks.unit_id','units.id')
+            ->join('colors','units.color_id','colors.id')
+            ->join('dealers','stocks.dealer_id','dealers.id')
+            ->select('units.model_name','colors.color_name','colors.color_code','units.year_mc','stocks.qty','dealers.dealer_code','dealers.dealer_name','stocks.id as id')
+            ->orderBy('stocks.qty','desc')
+            ->get();
+
             $data = Opname::where('opname_date',$today)->orderBy('id','desc')->get();
         }else{
-            $stock = Stock::where('dealer_id',$did)->orderBy('qty','desc')->get();
+            $stock = Stock::join('units','stocks.unit_id','units.id')
+            ->join('colors','units.color_id','colors.id')
+            ->join('dealers','stocks.dealer_id','dealers.id')
+            ->select('units.model_name','colors.color_name','colors.color_code','units.year_mc','stocks.qty','dealers.dealer_code','dealers.dealer_name','stocks.id as id')
+            ->where('stocks.dealer_id',$did)
+            ->orderBy('stocks.qty','desc')
+            ->get();
+            
             $data = Opname::join('stocks','opnames.stock_id','stocks.id')
             ->where('stocks.dealer_id',$did)
             ->where('opname_date',$today)->orderBy('opnames.id','desc')->get();
