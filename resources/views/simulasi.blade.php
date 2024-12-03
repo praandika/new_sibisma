a30682cb8b1e866d730a5b0537eb64080fd44154
Andika Pranayoga <praandikayoga@gmail.com>
2024-12-03 11:12:32 +0800

    Update Simulasi Kredit: Hapus bunga 1.75%

--- resources/views/simulasi.blade.php
+++ resources/views/simulasi.blade.php
@@ -146,7 +146,7 @@
                                         <option selected>Pilih Bunga</option>
                                         <option value="0.0240">2.4 %</option>
                                         <option value="0.0185">1.85 %</option>
-                                        <option value="0.0175">1.75 %</option>
+                                        <!-- <option value="0.0175">1.75 %</option> -->
                                     </select>
                                 </div>
 
