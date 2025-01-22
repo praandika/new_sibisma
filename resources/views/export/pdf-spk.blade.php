d1ccf23c6e0f336b6988df271a9e9a45db887a42
Andika Pranayoga <praandikayoga@gmail.com>
2025-01-22 09:52:01 +0800

    Update SPK Print add Year MC

--- resources/views/export/pdf-spk.blade.php
+++ resources/views/export/pdf-spk.blade.php
@@ -124,7 +124,7 @@
             </tr>
             <tr>
                 <th>Warna Motor</th>
-                <td>: {{ $o->stock->unit->color->color_faktur }}</td>
+                <td>: {{ $o->stock->unit->color->color_faktur }} &nbsp; ( {{ $o->stock->unit->year_mc }} )</td>
             </tr>
             <tr>
                 <th>Harga OTR</th>
